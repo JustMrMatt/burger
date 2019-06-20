311f1a6f3b05b584fc9b5a54344e7d124f7c6f43
Matthew Allen <matthewallenunc@gmail.com>
2019-06-19 22:56:30 -0400

    more setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("All-American Burger");
+INSERT INTO burgers (burger_name) VALUES ("Hickory Smokehouse Burger");
+INSERT INTO burgers (burger_name) VALUES ("The Garbage Burger");
